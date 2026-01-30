4e5bc96c6bd57b8abec0472bfc3a0215eb8cd5fb
Tasnif Ahmad <tasnif.hbtw@gmail.com>
2026-01-30 12:35:24 +0530

    Program to swap two numbers.

---
+++ SwapTwoNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        // Swapping using third variable
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+        Console.WriteLine("After Swapping:");
+        Console.WriteLine("First Number = " + num1);
+        Console.WriteLine("Second Number = " + num2);
+
+        Console.WriteLine("Press Enter to exit...");
+        Console.ReadLine();
+    }
+}
